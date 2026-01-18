6ef3c279ac30ae3a552202cdc5eff24a5763f6c8
unknown <murihat@gmail.com>
2026-01-18 10:16:58 +0700

    nomsg

--- src/app.js
+++ src/app.js
@@ -3,21 +3,10 @@ const path = require('path');
 
 const app = express();
 
-// =======================
-// MIDDLEWARE
-// =======================
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// =======================
-// STATIC FILES
-// =======================
 app.use(express.static(path.join(__dirname, '../public')));
 
-// =======================
-// ROUTER
-// =======================
-const router = require('./routers');
-app.use('/api', router);
+app.use('/api', require('./routers'));
 
 module.exports = app;
