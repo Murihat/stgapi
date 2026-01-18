22771f949d1c71bede2cdf39f513cf804ac5e943
unknown <murihat@gmail.com>
2026-01-18 10:30:45 +0700

    nomsg

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(__dirname + '/public'))
+
+// CORS setup
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: false
+}))
 
 app.use('/api', require('./routers'));
-
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
 module.exports = app;
