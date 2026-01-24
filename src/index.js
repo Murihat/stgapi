ccdd2c323b19b055579dba7ab5771807193ce508
unknown <murihat@gmail.com>
2026-01-24 14:48:27 +0700

    fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 const app = require('./app');
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Local API running at http://localhost:${PORT}`);
-});
-
-// const app = require('./app');
-
-// module.exports = app;
+module.exports = app;
