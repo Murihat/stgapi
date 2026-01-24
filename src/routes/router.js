d51c4a250dce5177c530c5aa88a4d80a2698e9ad
unknown <murihat@gmail.com>
2026-01-24 15:05:32 +0700

    router

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // PUBLIC ROUTES
-router.use('/api/brands', require('./brands.router'));
+router.use('/brands', require('./brands.router'));
 // router.use('/banners', require('./banners.router'));
 // router.use('/moments', require('./moments.router'));
 
